13e9c3de59ad50e33cad64465ea83ad62ef16412
Colin Clark <colin@clark.ws>
2017-06-20 14:45:32 +0200

    initial commit

---
+++ src/main/java/com/clark/kafkaconsumer/Main.java
@@ -0,0 +1,49 @@
+package com.clark.kafkaconsumer;
+
+/**
+ * Created by colin on 6/20/17.
+ */
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.*;
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    static {
+        PropertyConfigurator.configureAndWatch("log4j.properties", 5000);
+    }
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        final int minBatchSize = 50;
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<ConsumerRecord<String, String>>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                buffer.add(record);
+            }
+            if (buffer.size() >= minBatchSize) {
+                LOGGER.info("Inserting retrieved records into database and committing offset");
+                consumer.commitSync();
+                buffer.clear();
+            }
+        }
+    }
+}
